bd0353e495ff17467f8620556a570d1130a3b847
Gulara Nuri <ramazanova.gulare@gmail.com>
2022-11-24 00:43:53 +0400

    updating

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image.js');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : '1414',
-      database : 'smart-brain'
+      coonectiongString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
